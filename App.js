e5873eb0019fc7a1f54eb7153fff55279b3d0da9
dbdbd99 <dbdbd99@naver.com>
2021-07-19 11:18:34 +0900

    yes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>gik</Text>
+      <Text style={styles.text}>yes</Text>
       <StatusBar style="auto" />
     </View>
   );
